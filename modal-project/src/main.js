457fde5652c61376fca1ee9087b24bb98a40223d
Matthattan <116292730+Matthattan@users.noreply.github.com>
2024-03-17 01:14:21 +0000

    Use Header and Sidebar comps directly

--- modal-project/src/main.js
+++ modal-project/src/main.js
@@ -1,4 +1,9 @@
 import { createApp} from 'vue';
 import App from './App.vue';
+import Header from './components/Header.vue'
+import SideBar from './components/SideBar.vue';
 
-createApp(App).mount('#app');
+// createApp(App).mount('#app');
+
+createApp(Header).mount('#header');
+createApp(SideBar).mount('#sidebar')
