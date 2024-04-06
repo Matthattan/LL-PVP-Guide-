7683085953b61c8d71b944226844682984a76fa0
Matthattan <116292730+Matthattan@users.noreply.github.com>
2024-04-06 01:39:59 +0100

    (not) cooking

--- modal-project/src/main.js
+++ modal-project/src/main.js
@@ -4,11 +4,12 @@ import Header from './components/Header.vue'
 import SideBar from './components/SideBar.vue';
 import TextLine from './components/SelectText.vue'
 import Questions from './components/Questions.vue'
+import router from '../router'; // Import your router configuration
 
-createApp(App).mount('#app');
+
+createApp(App).use(router).mount('#app');
 createApp(TextLine).mount('#text')
 createApp(Questions).mount('#questions')
-
 
 //createApp(Header).mount('#header');
 //createApp(SideBar).mount('#sidebar')
