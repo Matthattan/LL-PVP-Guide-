df4721468669bf28d0ab3ada6b12a276b2ce842d
Matthattan <116292730+Matthattan@users.noreply.github.com>
2024-04-19 21:07:11 +0100

    Set Making & Credits

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,3 +1,29 @@
 <template>
     <router-view/>
 </template>
+<style>
+    * {
+        font-family: 'Russo One', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: black;
+    }
+
+    main {
+        font-size: 20px;
+        width: 60%;
+        margin: auto;
+        margin-bottom: 200px;
+    }
+
+    h1 {
+        margin: auto;
+    }
+
+    @media screen and (max-width: 300px){
+        h1 {
+            font-size: 18px;
+        }
+    }
+
+</style>
