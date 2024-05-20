e6b52d848a2f02b1e1051ccd0bae09e60ad0b906
Matthattan <116292730+Matthattan@users.noreply.github.com>
2024-05-20 19:14:31 +0100

    Update vue.config.js

--- modal-project/vue.config.js
+++ modal-project/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-  ? 'https://matthattan.github.io/LL-PVP-Guide-/'
+  ? 'https://matthattan.github.io/LL-PVP-Guide/'
   : '/',
   transpileDependencies: true
 })
